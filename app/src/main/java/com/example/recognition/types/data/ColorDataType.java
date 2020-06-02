f72a18c7baa7cdd9ef09fa8d8f6439d4214c07e1
RikkiTikki8 <RikkiTikki8@users.noreply.github.com>
2020-06-03 02:09:56 +0300

    update types of data

--- app/src/main/java/com/example/recognition/types/data/ColorDataType.java
+++ app/src/main/java/com/example/recognition/types/data/ColorDataType.java
@@ -1,4 +1,47 @@
 package com.example.recognition.types.data;
 
 public class ColorDataType {
+
+    public static class Color {
+
+        private String color;
+        private String name_color;
+        private double value;
+
+        public Color(String color, String name_color, double value) {
+            this.color = color;
+            this.name_color = name_color;
+            this.value = value;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+
+        public String getName_color() {
+            return name_color;
+        }
+
+        public void setName_color(String name_color) {
+            this.name_color = name_color;
+        }
+
+        public double getValue() {
+            return value;
+        }
+
+        public void setValue(double value) {
+            this.value = value;
+        }
+    }
+
+    protected ColorDataType.Color[] Colors;
+
+    public ColorDataType.Color[] getColors() {
+        return Colors;
+    }
 }
